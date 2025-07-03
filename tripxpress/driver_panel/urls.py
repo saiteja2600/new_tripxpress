c4a967b6181b67efef3b5308cbccbf56c30d701e
saiteja <85882586+saiteja2600@users.noreply.github.com>
2025-07-04 01:15:44 +0530

    today changes

--- tripxpress/driver_panel/urls.py
+++ tripxpress/driver_panel/urls.py
@@ -5,5 +5,14 @@ urlpatterns = [
     path('', views.driver_login, name='driver_login'),
     path('logout/', views.driver_logout, name='driver_logout'),
     path('change_password/', views.change_password, name='change_password'),
+    path('profile/', views.driver_profile, name='driver_profile'),
     path('Dashbord/', views.Dashbord, name='driver_dashbord'),
+    
+    # Vehicle
+    path('vehicle_information/', views.vehicle_information, name='vehicle_information'),
+    path('vehicle_maintenance/', views.vehicle_maintenance, name='vehicle_maintenance'),
+   path('Driver/vehicle_maintenance/delete/<int:service_id>/', views.vehicle_maintenance_delete, name='delete_maintenance'),
+
 ]
+
+
